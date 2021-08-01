e36d47ffc0979a3a3ff1d853df42df69aac59bc1
bbnumber2 <caleb.m.brinkman@gmail.com>
2021-07-31 22:20:10 -0500

    feat: Add constants file

---
+++ constant.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Display(Enum):
+    WINDOW_SIZE = (1000, 700)
+    BACKGROUND_COLOR = (30, 30, 30)
+    FOREGROUND_COLOR = (90, 90, 90)
+    FONT_FILENAME = 'Resources/LiberationSans-Regular.ttf'
+    FONT_SIZE = 16
