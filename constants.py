dc749ccf3ad11e698fcaad3e6f265443a55a87dd
bbnumber2 <caleb.m.brinkman@gmail.com>
2021-07-31 23:35:57 -0500

    chore: Add new simulation values to constants

--- constants.py
+++ constants.py
@@ -5,5 +5,9 @@ class Display(Enum):
     WINDOW_SIZE = (1000, 700)
     BACKGROUND_COLOR = (30, 30, 30)
     FOREGROUND_COLOR = (90, 90, 90)
+    FOREGROUND_LOCATION = (40, 90)
+    FOREGROUND_SIZE = (920, 550)
+    FOREGROUND_WIDTH = 2
+    FOREGROUND_RADIUS = 5
     FONT_FILENAME = 'Resources/LiberationSans-Regular.ttf'
     FONT_SIZE = 16
