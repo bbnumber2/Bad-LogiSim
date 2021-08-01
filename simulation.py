6a3a2b2209f280785aefe6bca07af2f47f52fbd6
bbnumber2 <caleb.m.brinkman@gmail.com>
2021-07-31 21:43:59 -0500

    feat: Add Simulation class to create the application window

---
+++ simulation.py
@@ -0,0 +1,14 @@
+from typing import Tuple
+import pygame
+
+
+class Simulation:
+    """
+    Creates the simulation window for the application.
+    """
+    def __init__(self,
+                 window_size: Tuple(int, int),
+                 background_color: Tuple(int, int, int),
+                 foreground_color: Tuple(int, int, int),
+                 font_filename: str) -> None:
+        pass
