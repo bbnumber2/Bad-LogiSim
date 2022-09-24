d985ba32bcc241566dab458552d7d812a046879d
bbnumber2 <caleb.m.brinkman@gmail.com>
2022-09-23 20:55:30 -0400

    refactor: Add separate event handling method

--- simulation.py
+++ simulation.py
@@ -10,6 +10,8 @@ class Simulation:
     def __init__(self) -> None:
         pygame.init()
         self.screen = None
+        self.textbook = None
+        self.objects = []
 
     def create_window(self) -> None:
         self.screen = pygame.display.set_mode(Display.WINDOW_SIZE)
@@ -19,45 +21,46 @@ class Simulation:
         pygame.draw.rect(self.screen, Display.FOREGROUND_COLOR,
                          foreground_rect, width=Display.FOREGROUND_WIDTH,
                          border_radius=Display.FOREGROUND_RADIUS)
+    
+    def handle_event(self, event: pygame.event) -> bool:
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            key = event.key
+            if key == pygame.K_ESCAPE:
+                return False
+            # Should be replaced with better keyboard in future
+            if self.textbox.selected:
+                if key == pygame.K_BACKSPACE:
+                    self.textbox.remove_character()
+                else:
+                    self.textbox.add_character(chr(key))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            button = event.button
+            pos = event.pos
+            if button == 1:
+                for object in self.objects:
+                    if object.rect.collidepoint(pos):
+                        object.click()
+                        continue
+                    object.unclick()
+        return True
 
     def start(self) -> None:
-        objects = []
         textbox_font = pygame.font.Font(Text.FONT_FILENAME, Text.FONT_SIZE)
-        textbox = Textbox(Text.TEXTBOX_POSITION, Text.TEXTBOX_SIZE,
+        self.textbox = Textbox(Text.TEXTBOX_POSITION, Text.TEXTBOX_SIZE,
                           Text.TEXTBOX_BACKGROUND_COLOR,
                           Text.TEXTBOX_OUTLINE_POSITION,
                           Text.TEXTBOX_OUTLINE_COLOR,
                           Text.TEXTBOX_OUTLINE_WIDTH,
                           Text.TEXTBOX_OUTLINE_RADIUS, textbox_font,
                           Text.COLOR, Text.TEXTBOX_TEXT_POSITION)
-        objects.append(textbox)
+        self.objects.append(self.textbox)
         running = True
         while running:
             for e in pygame.event.get():
-                if e.type == pygame.QUIT:
-                    running = False
-                    break
-                elif e.type == pygame.KEYDOWN:
-                    key = e.key
-                    if key == pygame.K_ESCAPE:
-                        running = False
-                        break
-                    # Should be replaced with more versatile keyboard in future
-                    if textbox.selected:
-                        if key == pygame.K_BACKSPACE:
-                            textbox.remove_character()
-                        else:
-                            textbox.add_character(chr(key))
-                elif e.type == pygame.MOUSEBUTTONDOWN:
-                    button = e.button
-                    pos = e.pos
-                    if button == 1:
-                        for object in objects:
-                            if object.rect.collidepoint(pos):
-                                object.click()
-                                continue
-                            object.unclick()
-            for object in objects:
+                running = self.handle_event(e)
+            for object in self.objects:
                 object.update()
                 object.render(self.screen)
             # Should be replaced with pygame.display.update() at some point
